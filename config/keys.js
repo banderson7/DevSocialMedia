5c40a106db9f7e44037409c3afeaf26459c56edd
Bill Anderson <rnthp7@gmail.com>
2018-09-01 10:45:28 -0500

    Added auth, GET current user

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://banders:Test!1234@ds139632.mlab.com:39632/mongo-dev1"
+  mongoURI: "mongodb://banders:Test!1234@ds139632.mlab.com:39632/mongo-dev1",
+  secretOrKey: "topsecret"
 };
