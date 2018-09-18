dfde68fa87ff2c580961f49bff10a85a3f9f1501
banders <banderson7@users.noreply.github.com>
2018-09-17 21:57:16 -0500

    Fixed Add Experience

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -85,7 +85,7 @@ export const getProfiles = () => dispatch => {
 export const addExperience = (expData, history) => dispatch => {
   axios
     .post("/api/profile/experience", expData)
-    .then(res => res.json())
+    .then(res => history.push("/dashboard"))
     .catch(err =>
       dispatch({
         type: GET_ERRORS,
